9681389f125d71978d4f815b478063934c4646a7
Tim Schoenborn <t.j.schonborn@gmail.com>
2022-02-11 16:34:09 +0100

    chunk_types.rs

---
+++ src/chunk_type.rs
@@ -0,0 +1,139 @@
+
+//    Copy the unit tests below and paste them at the bottom of your chunk_type.rs file.
+//    Write a ChunkType struct with your implementation of PNG chunk types.
+//    Implement TryFrom<[u8; 4]> for your ChunkType.
+//    Implement FromStr for your ChunkType.
+//    Implement Display for your ChunkType.
+//    Implement or derive PartialEq and Eq for your ChunkType
+//    Required methods:
+//        fn bytes(&self) -> [u8; 4]
+//        fn is_valid(&self) -> bool
+//        fn is_critical(&self) -> bool
+//        fn is_public(&self) -> bool
+//        fn is_reserved_bit_valid(&self) -> bool
+//        fn is_safe_to_copy(&self) -> bool
+//    Pass all of the unit tests.
+
+
+struct ChunkType {
+    pub data: [u8; 4];
+
+}
+
+impl ChunkType {
+
+    pub fn try_from(input: [u32; 4]) -> ChunkType {
+        for num in &input {
+            if (num < 65 || (num > 90 && num < 97) || num > 122) {
+                panic!("");
+            }
+            
+        }
+    }
+
+    pub fn from_str(input: &str) -> ChunkType {
+    
+    }
+
+
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::convert::TryFrom;
+    use std::str::FromStr;
+
+    #[test]
+    pub fn test_chunk_type_from_bytes() {
+        let expected = [82, 117, 83, 116];
+        let actual = ChunkType::try_from([82, 117, 83, 116]).unwrap();
+
+        assert_eq!(expected, actual.bytes());
+    }
+
+    #[test]
+    pub fn test_chunk_type_from_str() {
+        let expected = ChunkType::try_from([82, 117, 83, 116]).unwrap();
+        let actual = ChunkType::from_str("RuSt").unwrap();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    pub fn test_chunk_type_is_critical() {
+        let chunk = ChunkType::from_str("RuSt").unwrap();
+        assert!(chunk.is_critical());
+    }
+
+    #[test]
+    pub fn test_chunk_type_is_not_critical() {
+        let chunk = ChunkType::from_str("ruSt").unwrap();
+        assert!(!chunk.is_critical());
+    }
+
+    #[test]
+    pub fn test_chunk_type_is_public() {
+        let chunk = ChunkType::from_str("RUSt").unwrap();
+        assert!(chunk.is_public());
+    }
+
+    #[test]
+    pub fn test_chunk_type_is_not_public() {
+        let chunk = ChunkType::from_str("RuSt").unwrap();
+        assert!(!chunk.is_public());
+    }
+
+    #[test]
+    pub fn test_chunk_type_is_reserved_bit_valid() {
+        let chunk = ChunkType::from_str("RuSt").unwrap();
+        assert!(chunk.is_reserved_bit_valid());
+    }
+
+    #[test]
+    pub fn test_chunk_type_is_reserved_bit_invalid() {
+        let chunk = ChunkType::from_str("Rust").unwrap();
+        assert!(!chunk.is_reserved_bit_valid());
+    }
+
+    #[test]
+    pub fn test_chunk_type_is_safe_to_copy() {
+        let chunk = ChunkType::from_str("RuSt").unwrap();
+        assert!(chunk.is_safe_to_copy());
+    }
+
+    #[test]
+    pub fn test_chunk_type_is_unsafe_to_copy() {
+        let chunk = ChunkType::from_str("RuST").unwrap();
+        assert!(!chunk.is_safe_to_copy());
+    }
+
+    #[test]
+    pub fn test_valid_chunk_is_valid() {
+        let chunk = ChunkType::from_str("RuSt").unwrap();
+        assert!(chunk.is_valid());
+    }
+
+    #[test]
+    pub fn test_invalid_chunk_is_valid() {
+        let chunk = ChunkType::from_str("Rust").unwrap();
+        assert!(!chunk.is_valid());
+
+        let chunk = ChunkType::from_str("Ru1t");
+        assert!(chunk.is_err());
+    }
+
+    #[test]
+    pub fn test_chunk_type_string() {
+        let chunk = ChunkType::from_str("RuSt").unwrap();
+        assert_eq!(&chunk.to_string(), "RuSt");
+    }
+
+    #[test]
+    pub fn test_chunk_type_trait_impls() {
+        let chunk_type_1: ChunkType = TryFrom::try_from([82, 117, 83, 116]).unwrap();
+        let chunk_type_2: ChunkType = FromStr::from_str("RuSt").unwrap();
+        let _chunk_string = format!("{}", chunk_type_1);
+        let _are_chunks_equal = chunk_type_1 == chunk_type_2;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -12,5 +12,5 @@ fn main() -> Result<()> {
  }
 
 fn test() {
-}
+  }
 
